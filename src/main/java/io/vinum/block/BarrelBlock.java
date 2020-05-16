f9902dce0c4d8e2e7b824dfab67f680fe34c6d7e
Noah Martino <personal.boredhero@gmail.com>
2020-05-16 13:43:46 -0400

    Start work on Barrel TE

--- src/main/java/io/vinum/block/BarrelBlock.java
+++ src/main/java/io/vinum/block/BarrelBlock.java
@@ -1,6 +1,11 @@
 package io.vinum.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.common.extensions.IForgeBlock;
 
 public class BarrelBlock extends Block {
 	
@@ -8,5 +13,20 @@ public class BarrelBlock extends Block {
 		super(propertiesIn);
         
 	}
+
+	//This is depreciated. The docs must not be trusted I guess.
+	@Override
+	public boolean hasTileEntity(BlockState staet) {
+		return true;
+	}
+
+	//Can't override this even though the Docs mention it because it doesn't exist
+	/**
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+		//Return a new instance of the barrel tile entity.
+		return new TileEntity(TileEntityType.FURNACE);
+	}
+	*/
 	
 }
