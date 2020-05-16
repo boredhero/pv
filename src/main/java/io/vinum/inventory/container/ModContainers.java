a2edbc429330b26963e454886549d282a5f34aba
Rellit <tillereaton@gmail.com>
2020-05-16 13:26:29 -0700

    Fixed gui error

--- src/main/java/io/vinum/inventory/container/ModContainers.java
+++ src/main/java/io/vinum/inventory/container/ModContainers.java
@@ -1,6 +1,7 @@
 package io.vinum.inventory.container;
 
 import io.vinum.common.Defines;
+import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.fml.RegistryObject;
@@ -11,6 +12,6 @@ public class ModContainers {
 	
 	public static final DeferredRegister<ContainerType<?>> CONTAINERS = new DeferredRegister<>(ForgeRegistries.CONTAINERS, Defines.MODID);
 	
-	public static final RegistryObject<ContainerType<?>> STILL_MASTER = CONTAINERS.register("still_master", () -> IForgeContainerType.create(StillMasterContainer::new));
+	public static final RegistryObject<ContainerType<StillMasterContainer>> STILL_MASTER = CONTAINERS.register("still_master", () -> IForgeContainerType.create(StillMasterContainer::new));
 	
 }
